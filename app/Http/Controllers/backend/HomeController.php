321deb05d9c613ce52a1cb7aa54649719dc246b0
Fahim Hossain Munna <fahim.education.bd@gmail.com>
2024-09-13 00:52:25 +0600

    Total user login data

--- app/Http/Controllers/backend/HomeController.php
+++ app/Http/Controllers/backend/HomeController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers\backend;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 
 class HomeController extends Controller
 {
     public function index(){
-        return view('dashboard.home.index');
+        $users = User::all();
+        $today_logged_users = User::where('created_at', '>=', Carbon::now()->startOfDay())
+        ->where('created_at', '<=', Carbon::now()->endOfDay())->get();
+        return view('dashboard.home.index',compact('users','today_logged_users'));
     }
 }
--- resources/views/dashboard/home/index.blade.php
+++ resources/views/dashboard/home/index.blade.php
@@ -1,5 +1,55 @@
 @extends('layouts.master')
 
 @section('content')
+    <div class="row">
+        <div class="col-md-6 col-xl-3">
+            <div class="card">
+                <div class="card-body">
+                    <div class="mb-4">
+                        <span class="badge badge-soft-primary float-end">All Time</span>
+                        <h5 class="card-title mb-0">Total User</h5>
+                    </div>
+                    <div class="row d-flex align-items-center mb-4">
+                        <div class="col-8">
+                            <h2 class="d-flex align-items-center mb-0">
+                                {{ $users->count() }} Person
+                            </h2>
+                        </div>
+                    </div>
 
+                    <div class="progress shadow-sm" style="height: 5px;">
+                        <div class="progress-bar bg-success" role="progressbar" style="width: 100%;">
+                        </div>
+                    </div>
+                </div>
+                <!--end card body-->
+            </div><!-- end card-->
+        </div>
+        <div class="col-md-6 col-xl-3">
+            <div class="card">
+                <div class="card-body">
+                    <div class="mb-4">
+                        <span class="badge badge-soft-primary float-end">Today</span>
+                        <h5 class="card-title mb-0">Register User</h5>
+                    </div>
+                    <div class="row d-flex align-items-center mb-4">
+                        <div class="col-8">
+                            <h2 class="d-flex align-items-center mb-0">
+                                {{ $today_logged_users->count() }} Person
+                            </h2>
+                        </div>
+                        {{-- <div class="col-4 text-end">
+                            <span class="text-muted">12.5% <i class="mdi mdi-arrow-up text-success"></i></span>
+                        </div> --}}
+                    </div>
+
+                    <div class="progress shadow-sm" style="height: 5px;">
+                        <div class="progress-bar bg-success" role="progressbar" style="width: 100%;">
+                        </div>
+                    </div>
+                </div>
+                <!--end card body-->
+            </div><!-- end card-->
+        </div>
+    </div>
 @endsection
